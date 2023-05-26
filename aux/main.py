3acac1d23ea2f91aab2400e2fddeefc61c21442d
catamendizabal <catalina.mendizabal@ing.austral.edu.ar>
2023-05-26 17:57:59 -0300

    remove extra audios

--- aux/main.py
+++ aux/main.py
@@ -9,7 +9,7 @@ import whisper
 model = whisper.load_model("base")
 
 # load audio and pad/trim it to fit 30 seconds
-audio = whisper.load_audio("../audio-test.m4a")
+audio = whisper.load_audio("../audio.m4a")
 audio = whisper.pad_or_trim(audio)
 
 # make log-Mel spectrogram and move to the same device as the model
