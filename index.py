a1d14c09b1799433d0a943ac35ef86f94141fe0f
catamendizabal <catalina.mendizabal@ing.austral.edu.ar>
2023-06-18 12:31:54 -0300

    add openai model

--- index.py
+++ index.py
@@ -1,11 +1,14 @@
 import base64
 
 from flask import Flask, render_template, request
-import whisper
 import os
+from flask.cli import load_dotenv
+import openai
+
+load_dotenv()
 
 app = Flask(__name__)
-
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route('/')
 def voice_recognition_page():
@@ -24,14 +27,26 @@ def recognize_audio():
     with open('audio-temp.m4a', 'wb') as f:
         f.write(audio_data)
 
-    # load the model
-    model = whisper.load_model("base")
+    # load the model and transcribe the audio
+    # model = openai.Completion.create(
+    #     engine="davinci",
+    #     prompt="This is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to book a flight to Cairo.\nAI:",
+    #     temperature=0.9,
+    #     max_tokens=150,
+    #     top_p=1,
+    #     frequency_penalty=0.0,
+    #     presence_penalty=0.6,
+    #     stop=["\n", " Human:", " AI:"]
+    # )
+    # sin openai --> model = whisper.load_model("base")
 
     # load the audio
-    audio = whisper.load_audio("audio-temp.m4a")
+    # sin openai --> audio = whisper.load_audio("audio-temp.m4a")
+    audio_file = open("audio-temp.m4a", "rb")
+    result = openai.Audio.translate("whisper-1", audio_file)
 
     # transcribe audio
-    result = model.transcribe(audio)
+    # result = model.transcribe(audio)
 
     # get the transcription
     transcription = result["text"]
