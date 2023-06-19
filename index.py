5fbfa26591fdc2adda57812e3add51100ce31139
catamendizabal <catalina.mendizabal@ing.austral.edu.ar>
2023-06-19 11:49:33 -0300

    translation completed

--- index.py
+++ index.py
@@ -10,6 +10,7 @@ load_dotenv()
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+
 @app.route('/')
 def voice_recognition_page():
     return render_template('index.html')
@@ -17,36 +18,20 @@ def voice_recognition_page():
 
 @app.route('/recognize', methods=['POST'])
 def recognize_audio():
-    # get the request body
     body = request.get_json()
 
     # get the audio data and sample rate from the request
     audio_data = base64.b64decode(body['audio'])
 
     # save the audio data to a file
-    with open('audio-temp.m4a', 'wb') as f:
+    with open('tmp/audio-temp.m4a', 'wb') as f:
         f.write(audio_data)
 
-    # load the model and transcribe the audio
-    # model = openai.Completion.create(
-    #     engine="davinci",
-    #     prompt="This is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to book a flight to Cairo.\nAI:",
-    #     temperature=0.9,
-    #     max_tokens=150,
-    #     top_p=1,
-    #     frequency_penalty=0.0,
-    #     presence_penalty=0.6,
-    #     stop=["\n", " Human:", " AI:"]
-    # )
-    # sin openai --> model = whisper.load_model("base")
-
     # load the audio
-    # sin openai --> audio = whisper.load_audio("audio-temp.m4a")
-    audio_file = open("audio-temp.m4a", "rb")
-    result = openai.Audio.translate("whisper-1", audio_file)
+    audio_file = open("tmp/audio-temp.m4a", "rb")
 
-    # transcribe audio
-    # result = model.transcribe(audio)
+    # transcribe audio to spanish
+    result = openai.Audio.translate("whisper-1", audio_file)
 
     # get the transcription
     transcription = result["text"]
@@ -58,10 +43,12 @@ def recognize_audio():
     }
 
     # delete the temporary audio file
-    os.remove('audio-temp.m4a')
+    os.remove('tmp/audio-temp.m4a')
 
-    # return the recognized text
     return response_data
+
+
+# make function to run agent with openai and
 
 
 if __name__ == '__main__':
